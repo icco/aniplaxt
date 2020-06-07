501cc7e7950e8e83c0b3b5945bcc261407b0b69b
Nat Welch <nat@natwelch.com>
2020-06-07 02:42:56 +0000

    stub out anilist

---
+++ lib/anilist/anilist.go
@@ -0,0 +1,19 @@
+package anilist
+
+import (
+	"github.com/icco/aniplaxt/lib/store"
+	"github.com/xanderstrike/plexhooks"
+)
+
+func AuthRequest(root, username, code, refreshToken, grantType string) map[string]interface{} {
+	return nil
+}
+
+func Handle(pr plexhooks.PlexResponse, user store.User) {
+}
+
+func HandleMovie(pr plexhooks.PlexResponse, accessToken string) {
+}
+
+func HandleShow(pr plexhooks.PlexResponse, accessToken string) {
+}
