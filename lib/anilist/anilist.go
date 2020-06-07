1cfa3774b39d51be658ea239acc19477508822d1
Nat Welch <nat@natwelch.com>
2020-06-07 23:06:26 +0000

    no more

--- lib/anilist/anilist.go
+++ lib/anilist/anilist.go
@@ -1,51 +1,12 @@
 package anilist
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
 
 	"github.com/icco/aniplaxt/lib/store"
 	"github.com/xanderstrike/plexhooks"
 )
-
-// AuthRequest parses the auth request to anilist.
-func AuthRequest(root, username, code, refreshToken, grantType string) (map[string]string, error) {
-	values := map[string]string{
-		"code":          code,
-		"refresh_token": refreshToken,
-		"client_id":     os.Getenv("ANILIST_ID"),
-		"client_secret": os.Getenv("ANILIST_SECRET"),
-		//"redirect_uri":  fmt.Sprintf("%s/authorize?username=%s", root, url.PathEscape(username)),
-		"redirect_uri": fmt.Sprintf("%s/authorize", root),
-		"grant_type":   grantType,
-	}
-	jsonValue, err := json.Marshal(values)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := http.Post("", "application/json", bytes.NewBuffer(jsonValue))
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode > 200 {
-		//log.WithField("response", resp).Warnf("got a %q. Aborting to avoid panic.", resp.Status)
-		return nil, fmt.Errorf("could not contact anilist API: %+v", resp)
-	}
-
-	var result map[string]string
-	err = json.NewDecoder(resp.Body).Decode(&result)
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
 
 // Handle decides what API calls to make based off of the incoming Plex
 // webhook.
